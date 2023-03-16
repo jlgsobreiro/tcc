f30b8ac355f935445189849a16b62342a0185968
José <jlgsobreiro@gmail.com>
2023-03-16 19:51:36 -0300

    cleaning simple_crud

--- simple_crud/models/Insumos.py
+++
@@ -1,8 +0,0 @@
-from api.models.Base import Base
-from api.models.Produto import Produto
-from mongoengine import Document
-from mongoengine.fields import *
-
-
-class Insumos(Document):
-    insumo = StringField()
